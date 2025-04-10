ac22b3d56c5618f5759669f8ec8d28b726f263fb
Seydina Alioune Mboup <aliounembp3k@gmail.com>
2025-04-10 01:44:58 +0000

    Take the viewa of all products

--- Produits/views.py
+++ Produits/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from .models import *
+
+from django.views.generic import ListView
 
 # Create your views here.
 def home(request):
+
+    # Recuperations des donnees de la bd
+    #donnees = Produits.objects.all()
+
+    #context = {
+    #    'donnees':donnees
+    #}
+
     return render(request,"home.html")
+
+class Affichage(ListView):
+    # Affichage du template
+    template_name = 'home.html'
+    # Recuperer les donnees de la bd
+    queryset = Produits.objects.all()
